5ccbf1f2da1df803d9499593d7aa6982f458ceb3
Ravuthz <ravuthz@gmail.com>
2017-07-23 14:18:44 +0700

    Configure Authentication with Authorize (@PreAuthorize)

--- src/main/java/com/ravuthz/springboot/review/ReviewRepository.java
+++ src/main/java/com/ravuthz/springboot/review/ReviewRepository.java
@@ -1,6 +1,8 @@
 package com.ravuthz.springboot.review;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 /**
  * Created by Vannaravuth Yo
@@ -8,4 +10,11 @@ import org.springframework.data.repository.PagingAndSortingRepository;
  * Email : ravuthz@gmail.com
  */
 public interface ReviewRepository extends PagingAndSortingRepository<Review, Long> {
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN') or @reviewRepository.findOne(#id)?.reviewer?.username == authentication.name")
+    void delete(@Param("id") Long id);
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN') or #review.reviewer?.username == authentication.name")
+    void delete(@Param("review") Review review);
 }
